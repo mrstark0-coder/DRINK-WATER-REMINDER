4482d43aadf86b8354d95273c242e84427953bd0
Bhaskar Arya <bhashkararya2003@gmail.com>
2025-07-18 17:22:49 +0530

    fist commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import pyttsx3
+import time
+from plyer import notification
+
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+while True:
+    notification.notify(
+        title = "Drink Water",
+        message = "Take a few sips of water now",
+        timeout = 30
+        )
+
+    
+    speak("Drink Water PLease")
+
+    time.sleep(30)
+    
+
+    
+
+
